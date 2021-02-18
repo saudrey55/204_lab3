5d96457dcff8624a9bbf20329d908ac22b7f5fa9
saudrey <weerawutearth@gmail.com>
2021-02-18 17:23:27 +0700

    c

--- 6220502183_3_5.c
+++ 6220502183_3_5.c
@@ -3,17 +3,36 @@
 int funtion (int x)
 {
     int i;
-
+    if (x==1)
+    {
+        return 0;
+    }
+    
     for ( i = 2; i < x; i++)
     {
         if (x%i==0)
         {
-           return 0;
+           return x/i;
         }
     }
     return 1;
 }
 int main()
 {
-    scanf("")
+    int a,ch,i,z;
+    scanf("%d",&a);
+    z=a;
+    ch=1;
+    for ( i = 0; i < 3; i++)
+    {
+        z=funtion(z);
+    }
+    if (z==1)
+    {
+        printf("%d is a Lucky Number.",a);
+    }
+    else
+        printf("%d is not a Lucky Number.",a);
+    
+    
 }
