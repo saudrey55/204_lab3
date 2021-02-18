7a987d70ba0d96d8e23a8e2a04eb2e7f148944dd
saudrey <weerawutearth@gmail.com>
2021-02-18 16:33:58 +0700

    Update 6220502183_3_1.c

--- 6220502183_3_1.c
+++ 6220502183_3_1.c
@@ -2,17 +2,21 @@
 
 int main()
 {
-  int a[5][4],i,j;
+  int n[5][4],i,j,a[20],max,z;
 
   for ( i = 0; i <5; i++)
   {
-      for ( j = 0; j < 4; j++)
+    for ( j = 0; j < 4; j++)
       {
-          scanf("%d",&a[i][j]);
+          scanf("%d",&n[i][j]);
+          a[i]=a[i]+n[i][j];
+          if(a[i]>max)
+          {
+           max =a[i];
+            z=i;
+          }
       }
-      
   }
-
+ printf("%d %d",z+1,max);
   
-     
 }
